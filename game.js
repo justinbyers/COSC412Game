10bb3fb383a0c21c0e57333cff0e042d17902fed
justinbyers <byersjustin3@gmail.com>
2019-03-20 13:57:58 -0400

    game.js changes, css additions

--- game.js
+++ game.js
@@ -1,5 +1,21 @@
 // this is super unfinished
 //all work as of now is done by Justin Byers
+//github repos @ https://github.com/justinbyers/COSC412Game
+
+/* TODO:
+    -fix restart() so that the pause/resume correctly resets
+    -remove excess console.logs that have no purpose anymore
+    -implement some enemy/monster variable to control x and y coords (instances)
+    -figure out how to correctly implement some sort of pathing function
+        (current pathing will endlessly loop if there is 2+ left or up moves)
+    -give up
+*/
+
+/*
+helpful resources: 
+https://github.com/Trimps/Trimps.github.io
+http://dhmholley.co.uk/incrementals.html
+*/
 
 var gameData = {
         money: 0,
@@ -13,19 +29,23 @@ var gameData = {
         npc2: 0,
 
     };
+    var continueMoving = true;
 
-window.addEventListener('load', loadup, false);
+window.addEventListener('load', loadup, false); //run loadup() when page loads
 
-var c = 0;
+var c = 0; //obsolete i think
 
+//tick function
 window.setInterval(function(){
     this.gameData.time += 1;
     updateTime();
-    move();
-    
-}, 1000);
+    if(continueMoving)
+        move();
 
-var templateGrid =
+    
+}, 250); //250 = 4 ticks every second, 1000 = 1 tick every second. 1000ms in a sec, 4x250=1000
+
+var templateGrid2 = //10x10 -- not used because it has left and up movements which the current code cant comprehend correctly
 [[false, false, false, false, false, false, false, false, false, false],
 [true, true, true, true, false, false, true, true, true, false],
 [false, false, false, true, false, false, true, false, true, false],
@@ -37,9 +57,47 @@ var templateGrid =
 [false, true, true, true, true, false, false, true, false, false],
 [false, false, false, false, false, false, false, true, false, false]];
 
+var templateGrid = //10x10 currently used
+[[false, false, false, false, false, false, false, false, false, false],
+[true, true, true, true, false, false, false, false, false, false],
+[false, false, false, true, true, false, false, false, false, false],
+[false, false, false, false, true, true, true, false, false, false],
+[false, false, false, false, false, false, true, false, false, false],
+[false, false, false, false, false, false, true, false, false, false],
+[false, false, false, false, false, true, true, false, false, false],
+[false, false, false, false, false, true, false, false, false, false],
+[false, false, false, false, false, true, false, false, false, false],
+[false, false, false, false, false, true, false, false, false, false]];
+
+var templateGrid1 = //50x10 -- not used
+[[false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+[true, true, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+[false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+[false, false, false, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+[false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+[false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+[false, false, false, false, false, false, false, true, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+[false, false, false, false, false, false, false, false, false, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+[false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+[false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]];
+
+var templateGrid3 = //100x10 -- not used
+[[false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+[true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+[false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+[false, false, false, false, false, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+[false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+[false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+[false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+[false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+[false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+[false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]];
+
 var cols = 10;
 var rows = 10;
+var tileSize = 20;
 
+//creates a quick little grid using variables col,rows,tileSize
 function createGrid(){
     var grid = document.getElementById("grid");
     grid.className = "";
@@ -55,16 +113,16 @@ function createGrid(){
         row.style.paddingBottom = 0 + "px";
         row.style.margin = "0px 0px 0px 0px";
         row.style.listStyle = 0; 
-        row.style.width = cols * 10 + "px";
-        row.style.height = cols + "px";
+        row.style.width = cols * tileSize + "px";
+        row.style.height = tileSize + "px";
         
         for(var j = 0; j < cols; j++){
             var cell = document.createElement("li");
-            cell.setAttribute("id", cell + counter)
+            cell.setAttribute("id", "cell" + counter)
             row.appendChild(cell);
 
-            cell.style.width = 10 + "px";
-            cell.style.height = 10 + "px";
+            cell.style.width = tileSize + "px";
+            cell.style.height = tileSize + "px";
             cell.style.backgroundColor = "#228b22";
 
             
@@ -80,7 +138,7 @@ function createGrid(){
     } 
 }
 
-
+//returns the x or y start depending on input "x" or "row" vs "y" or "col"
 function findStart(type){
     //tries to begin a move from (x, y) 
     //probably (1, 0) row 1, col 0
@@ -88,7 +146,6 @@ function findStart(type){
     switch(type){
         case ("x"):
         case ("row"):
-        console.log("X OR ROW");
         for(var i = 0; i < rows; i++){
             //console.log(templateGrid[i][0] + " ");
             if(templateGrid[i][0] == true){
@@ -104,7 +161,6 @@ function findStart(type){
 
         case ("y"):
         case ("col"):
-        console.log("Y OR COL");
         for(var i = 0; i < rows; i++){
             //console.log(templateGrid[i][0] + " ");
             if(templateGrid[i][0] == true){
@@ -124,6 +180,8 @@ function findStart(type){
 
 }
 
+//doodoo global variables for x and y coords
+//really should be attached to monster/enemy instances
 var x = this.startX;
 var y = this.startY;
 
@@ -134,34 +192,64 @@ var y = this.startY;
 //also x and y coords should be resolved to some sort of
 // moster/enermy/??? class-ish variable, so that each instance of enemy holds x and y
 function move(){
+    document.getElementById(("cell" + (x) + y)).style.backgroundColor = "#222222";
 
-    console.log(templateGrid[x][y]);
-    if(templateGrid[x+1][y] == true){
-        console.log("can move 1 down");
+    // if(y < 10)
+    //     document.getElementById(("cell" + (x*10) + y)).style.backgroundColor = "#222222"; //*5 because there's 50 cols rather than 10
+    // else
+    //     document.getElementById(("cell" + (x) + y)).style.backgroundColor = "#222222"; //*5 because there's 50 cols rather than 10
+    
+    console.log("GRID" + templateGrid[x][y]);
+    if(x + 1 == 9){
         x += 1;
+        console.log("exit reached " + x + " " + y);
+        continueMoving = false;
+    }
+    else if(templateGrid[x+1][y] == true){        
+        x += 1;
+        console.log("can move 1 down " + x + " " + y);
+
     }
     else if(templateGrid[x][y+1] == true){
-        console.log("can move 1 right");
         y += 1;
-    }
-    else if(templateGrid[x-1][y] == true){
-        console.log("can move 1 up");
-        x -= 1;
+        console.log("can move 1 right " + x + " " + y);
     }
     else if(templateGrid[x][y-1] == true){
-        console.log("can move 1 left");
         y -= 1;
+        console.log("can move 1 left " + x + " " + y);
     }
+    else if(templateGrid[x-1][y] == true){
+        console.log("can move 1 up but do nothing");
+        //x -= 1;
+    }
+    else{
+        console.log("EXIT REACHED");
+        continueMoving = false;
+    }
+    
 
-       // c++;
+    //update cell[x][y]
+    console.log("x: " + x + " y: " + y + "   " + "cell" + (x) + y + " ");
+    // if(y < 10)
+    // console.log("x: " + x + " y: " + y + "   " + "cell" + (x*10) + y + " ");
+    // else
+    // console.log("x: " + x + " y: " + y + "   " + "cell" + (x) + y + " ");
+    
+    document.getElementById(("cell" + (x) + y)).style.backgroundColor = "#EEEEEE";
+    // if(y < 10)
+    //     document.getElementById(("cell" + (x*10) + y)).style.backgroundColor = "#EEEEEE"; //*5 because there's 50 cols rather than 10
+    // else
+    //     document.getElementById(("cell" + (x) + y)).style.backgroundColor = "#EEEEEE"; //*5 because there's 50 cols rather than 10
     
 }
 
+//--
 function displayGrid(){    
 }
 
+//funct ran on loadup
 function loadup(){
-    document.body.style.backgroundColor = "#666666";
+    document.body.style.backgroundColor = "#666666"; //random color, easier to work with in a dark room
 
     // gameData = {
     //     money: 0,
@@ -177,11 +265,49 @@ function loadup(){
     createGrid();
     this.gameData.startX = findStart("x");
     this.gameData.startY = findStart("y");
-
+    console.log(gameData.startX + " " + gameData.startY);
+    // if(y < 10)
+    //     document.getElementById(("cell" + (x*10) + y)).style.backgroundColor = "#EEEEEE"; //*5 because there's 50 cols rather than 10
+    // else
+    //     document.getElementById(("cell" + (x) + y)).style.backgroundColor = "#EEEEEE"; //*5 because there's 50 cols rather than 10
+    document.getElementById(("cell" + x + y)).style.backgroundColor = "#EEEEEE";
     //move();
 
 }
 
+//updates time elapsed
 function updateTime(){
     document.getElementById("time").innerHTML = this.gameData.time;
+}
+
+
+//stops the grid movement 
+//(currently doesnt update button .innerhtml correctly)
+function stopMoving(){
+    continueMoving = !continueMoving;
+    
+    var flag = document.getElementById("btn2").innerHTML;
+    console.log(flag);
+    switch(flag.innerHTML){
+    case ("Resume"):
+        flag.innerHTML = "Pause";
+        break;
+    case ("Pause"):
+        flag.innerHTML = "Resume";
+        break
+    default:
+        console.log("Error stopmoving() switch case");
+    }
+    // console.log(flag);
+}
+
+//restarts the grid animation -- works
+function restart(){
+    console.log("restarted");
+    createGrid();
+    this.gameData.startX = findStart("x");
+    this.gameData.startY = findStart("y");
+    console.log(gameData.startX + " " + gameData.startY);
+    document.getElementById(("cell" + x + y)).style.backgroundColor = "#EEEEEE";
+
 }
